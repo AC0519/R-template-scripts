fc5a61a7b8aea5a457bb45de79c65d8810074227
AC0519 <30775693+AC0519@users.noreply.github.com>
2018-11-13 07:25:12 -0500

    Add files via upload

--- pdf_scrape.R
+++ pdf_scrape.R
@@ -1,9 +1,21 @@
+#The goal is to extract all of the speeches from the document except the presidents.
+
 library(pdftools)
 library(tm)
 library(magrittr)
 
+#Pull in the document and split it on the line breaks
 text <- pdf_text("71_PV.62.pdf") 
-        
-text <- strsplit(text, "\n")
+text <- strsplit(text, "\r\n")
 
-head(text[[1]])
+#View the head of the first page
+tail(text[[1]],10)
+
+
+#remove header on the first page
+#president_row <- grep("^President:", text)[1]
+#text <- text[(president_row + 1):length(text)]
+
+
+#remove footer on the first page
+text <- gsub("This record contains.*", text)
